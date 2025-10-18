998c2d95975476e6805808374f4d075f5ff80a46
Forhad <71933188+Forhad700@users.noreply.github.com>
2025-10-18 12:45:11 +0600

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import os
 import streamlit as st
 from dotenv import load_dotenv
 from langchain_community.vectorstores import FAISS
-from langchain.chains.retrieval import create_retrieval_chain
+from langchain.chains import create_retrieval_chain
 from langchain.chains.combine_documents import create_stuff_documents_chain
 from langchain_core.prompts import ChatPromptTemplate
 from langchain_groq import ChatGroq
